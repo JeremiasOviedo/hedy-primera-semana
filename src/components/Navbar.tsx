d55fd40971cb6268da00a767c94d5f82daca4e3f
JeremiasOviedo <JeremiasOviedo.Dev@gmail.com>
2024-03-22 19:12:47 -0300

    Small adjustments in the navbar links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,29 +8,27 @@ const Navbar: React.FC = () => {
     <div className="flex justify-between items-center px-2 w-full h-20  text-white bg-gradient-to-r from-violet-400  to-violet-500 fixed">
       <div>
         <h1 className="text-3xl font-sans font-bold ml-2">
-          <Link to="/">     
+          <Link to="/">
             <div className="flex">
               <span className="mx-4">Bookers</span>
               <BsBookmarkHeartFill />
             </div>
-            </Link>
+          </Link>
         </h1>
       </div>
       <div>
         <ul className="hidden md:flex h-full items-center">
-          <li className="nav-links px-4 cursor-pointer capitalize font-medium text-gray-100 hover:scale-105 hover:text-white duration-200 link-underline text-left">
+          <li className="nav-links px-4 capitalize font-medium text-gray-100 hover:scale-105 hover:text-white duration-200 link-underline text-left">
             <Link to="/">Home</Link>
           </li>
-          <li className="nav-links px-4 cursor-pointer capitalize font-medium text-gray-100 hover:scale-105 hover:text-white duration-200 link-underline text-left">
+          <li className="nav-links px-4 capitalize font-medium text-gray-100 hover:scale-105 hover:text-white duration-200 link-underline text-left">
             <Link to="/events">Eventos</Link>
           </li>
-          <li className="nav-links px-4 cursor-pointer capitalize font-medium text-gray-100 hover:scale-105 hover:text-white duration-200 link-underline text-left">
+          <li className="nav-links px-4 capitalize font-medium text-gray-100 hover:scale-105 hover:text-white duration-200 link-underline text-left">
             <Link to="/contact">Contacto</Link>
           </li>
         </ul>
       </div>
-
-      <ul className="hidden md:flex h-full items-center"></ul>
 
       <div
         onClick={() => setNav(!nav)}
@@ -40,13 +38,13 @@ const Navbar: React.FC = () => {
       </div>
       {nav && (
         <ul className="flex flex-col mt-[80px] justify-top items-left absolute top-0 left-0 w-full h-[400px] bg-gradient-to-r from-violet-400 to-violet-500">
-          <li className="mx-4 cursor-pointer capitalize py-6 text-xl text-gray-100 hover:text-white duration-200 link-underline text-left">
+          <li className="mx-4 capitalize py-6 text-xl text-gray-100 hover:text-white duration-200 link-underline text-left">
             <Link to="/">Home</Link>
           </li>
-          <li className="mx-4 cursor-pointer capitalize py-6 text-xl text-gray-100 hover:text-white duration-200 link-underline text-left">
-          <Link to="/events">Eventos</Link>
+          <li className="mx-4  capitalize py-6 text-xl text-gray-100 hover:text-white duration-200 link-underline text-left">
+            <Link to="/events">Eventos</Link>
           </li>
-          <li className="mx-4 cursor-pointer capitalize py-6 text-xl text-gray-100 hover:text-white duration-200 link-underline text-left">
+          <li className="mx-4  capitalize py-6 text-xl text-gray-100 hover:text-white duration-200 link-underline text-left">
             <Link to="/contact">Contacto</Link>
           </li>
         </ul>
